f8fddec5785bbc75036ab3d8c8dc52003ea304c5
aarnav-art <84204386+aarnav-art@users.noreply.github.com>
2021-05-18 15:09:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//function decolor(){
+//    document.getElementById("irt").style.color="pink";
+//    
+//}
+//function setDate(){
+//    document.getElementById("delta").innerHTML=new Date().toDateString();
+//}
+//function decolor(){
+//    document.getElementById("irt").style.color="pink";
+//    
+//}
+//function setDate(){
+//    document.getElementById("delta").innerHTML=new Date().toDateString();
+//}
